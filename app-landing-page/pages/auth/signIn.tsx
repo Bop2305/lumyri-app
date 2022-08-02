9626bfa0844acde09cc9c6f73f3d497a4ab7298e
thongnc <canhthong05@gmail.com>
2022-08-02 21:32:59 +0700

    update UI Sign In

--- app-landing-page/pages/auth/signIn.tsx
+++ app-landing-page/pages/auth/signIn.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "../../styles/SignIn.module.scss";
-import Footer from "../../components/layout/Footer";
+import Footer from "../../components/auth/Footer";
 import { Button, Form } from "react-bootstrap";
 
 const SignIn = () => {

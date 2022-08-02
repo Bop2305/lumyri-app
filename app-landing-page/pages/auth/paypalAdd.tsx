fa384a0877aca824d139c88910fe6c026ca32738
thongnc <canhthong05@gmail.com>
2022-08-02 23:01:46 +0700

    add UI Paypal Add

---
+++ app-landing-page/pages/auth/paypalAdd.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import styles from "../../styles/Verify.module.scss";
+
+const PaypalAdd = () => {
+  return (
+    <div className={styles.background}>
+      <Container>
+        <h2>Paypal Add</h2>
+      </Container>
+    </div>
+  );
+};
+
+export default PaypalAdd;
